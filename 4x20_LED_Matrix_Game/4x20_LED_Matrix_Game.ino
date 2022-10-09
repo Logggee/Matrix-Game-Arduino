2a5e4e7cee397d26081132a4cd9eae4e5b954ff7
Jan Holderied <holderja@hs-albsig.de>
2022-10-09 17:23:18 +0200

    all Pin defines and pinMode setups in void setup

--- 4x20_LED_Matrix_Game/4x20_LED_Matrix_Game.ino
+++ 4x20_LED_Matrix_Game/4x20_LED_Matrix_Game.ino
@@ -1,7 +1,16 @@
+#define LATCH 9
+#define CLOCK 10
+#define DATA 8
+#define SERCLK 3
+#define OE 2
 
 void setup() 
 {
-
+  pinMode(LATCH,OUTPUT);
+  pinMode(CLOCK,OUTPUT);
+  pinMode(DATA,OUTPUT);
+  pinMode(SERCLK,OUTPUT);
+  pinMode(OE,OUTPUT);
 }
 
 void loop() 
