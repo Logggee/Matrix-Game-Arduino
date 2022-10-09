3f8d1228f4b9233f7acf3ab3b13fd4b6e51ca41f
Jan Holderied <holderja@hs-albsig.de>
2022-10-09 20:12:57 +0200

    Display Funktion hinzugefügt mit der nun 3 bytes in die register und eine Anzeige Zeit als Parameter übergeben werden soll

--- 4x20_LED_Matrix_Game/4x20_LED_Matrix_Game.ino
+++ 4x20_LED_Matrix_Game/4x20_LED_Matrix_Game.ino
@@ -15,5 +15,21 @@ void setup()
 
 void loop() 
 {
+  Display(0x3C, 0x00, 0x00, 1000);
+}
 
+void Display(byte byte1, byte byte2, byte byte3, byte displayTime)
+{
+  digitalWrite(SERCLK,HIGH);
+  digitalWrite(OE,HIGH);
+  digitalWrite(LATCH,LOW);
+  shiftOut(DATA,CLOCK,MSBFIRST,byte1);
+  shiftOut(DATA,CLOCK,MSBFIRST,byte2);
+  shiftOut(DATA,CLOCK,MSBFIRST,byte3);
+  digitalWrite(LATCH,HIGH);
+  digitalWrite(OE,LOW);
+  delay(displayTime);
+  digitalWrite(SERCLK,LOW);
+  digitalWrite(LATCH,LOW);
+  digitalWrite(LATCH,HIGH);
 }
